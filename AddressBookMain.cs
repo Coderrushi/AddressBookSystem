2276a586a34c16be35fe6ff84c39c3185983f41e
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-10-28 20:26:27 +0530

    added AddressBookMain class

---
+++ AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System");
+        }
+    }
+}
